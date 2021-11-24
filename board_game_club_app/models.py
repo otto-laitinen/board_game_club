178092e278bee1f4d475caa1f48a178e3e4b3932
Otto <otto.laitinen@edu.turkuamk.fi>
2021-11-24 11:56:21 +0200

    added BoardGame class

--- board_game_club_app/models.py
+++ board_game_club_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class BoardGame(models.Models):
+    """A board game and it's information."""
+    name = models.Charfield(max_length=150)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns a string representation of the model."""
+        return self.name
