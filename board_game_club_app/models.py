315bf456f449c4641b8e3c5b238886fb4e478594
Otto <otto.laitinen@edu.turkuamk.fi>
2021-12-01 18:22:19 +0200

    modified availability field

--- board_game_club_app/models.py
+++ board_game_club_app/models.py
@@ -8,6 +8,7 @@ class BoardGame(models.Model):
     description = models.TextField(max_length=400)
     date_added = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
 
     def __str__(self):
         """Returns a string representation of the model."""
