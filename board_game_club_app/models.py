b1d93ec15855c34123997aa92ade855468cd4bb9
Otto <otto.laitinen@edu.turkuamk.fi>
2021-12-01 17:53:42 +0200

    added description and availability fields

--- board_game_club_app/models.py
+++ board_game_club_app/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class BoardGame(models.Model):
     """A board game and it's information."""
+
     name = models.CharField(max_length=150)
+    description = models.TextField(max_length=400)
     date_added = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now_add=True)
 
