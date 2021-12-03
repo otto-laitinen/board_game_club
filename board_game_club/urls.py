97fa1184b9fd167628b118b37706521189377274
Eyal Davidi <eyald20>
2021-12-03 18:08:11 +0200

    adding user privleges

--- board_game_club/urls.py
+++ board_game_club/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("board_game_club_app.urls")),
+
+    # Page for adding a new board game
+path('new_board_game/', views.new_board_game, name = 'new_board_game'),
 ]
